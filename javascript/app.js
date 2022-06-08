4976f0d5d57678f1758bafffc8885513788642a0
lcedrez <102386169+lcedrez@users.noreply.github.com>
2022-06-08 18:20:39 -0300

    Img

--- javascript/app.js
+++ javascript/app.js
@@ -21,10 +21,15 @@ class Usuario{
 }
 
 
-const articulo1= new Articulo("1144","Let It Be Special Edition – Standard LP<",2500,0,"Imagenes/E-Commerce/LetitbeCD.jpg")
+const articulo1= new Articulo("1144","Let It Be Special Edition – Standard LP",2500,0,"Imagenes/E-Commerce/LetitbeCD.jpg")
 const articulo2= new Articulo("2244","Let It Be Embroidered Blue Crewneck",4200,0,"Imagenes/E-Commerce/LetitBeCanguro.jfif")
 const articulo3= new Articulo("3344","Let It Be Photo Red Crewneck",5200,0,"Imagenes/E-Commerce/LetItbeCanguroRojojfif.jfif")
-const articulo4= new Articulo("4444","Let It Be Silver Vintage Black T-Shirt",8250,0,"Imagenes/E-Commerce/LetItbeCanguroRojojfif.jfif")
+const articulo4= new Articulo("4444","Launchera - The Beatles",8250,0,"Imagenes/E-Commerce/Launchera.jfif")
+const articulo5=new Articulo("5544","Beatles Playing Cards - Special Edition Box Set",1250,0,"Imagenes/E-Commerce/BeatlesCard.jpg")
+const articulo6=new Articulo("6644","Abbey Road Ukulele",17500,0,"Imagenes/E-Commerce/UkuleleAbbey.jfif")
+const articulo7=new Articulo("7744","Yellow Submarine Port Hole Beanie",1500,0,"Imagenes/E-Commerce/gorroYsub.jfif")
+const articulo8=new Articulo("8844","I Love The Beatles Pin",200,0,"Imagenes/E-Commerce/PIN.jfif")
+
 
 const usuario1=new Usuario("Luciano","Cedrez","lcedrez","Luciano1234")
 const usuario2=new Usuario("Juan","Perez","jperez","Juan1234567")
@@ -34,9 +39,39 @@ const usuario4=new Usuario("Luis","Martinez","lmartinez","Luis4545")
 
 
 let totalFinal=0
-const artVentas=[articulo1,articulo2,articulo3,articulo4 ]
+const artVentas=[articulo1,articulo2,articulo3,articulo4,articulo5,articulo6,articulo7,articulo8]
 const usuSistema=[usuario1,usuario2,usuario3,usuario4]
 const carrito=[]
+
+
+
+
+
+    
+    //Primero tenemos que capturar la card y guardarla en una variable
+
+    const contenedorCard=document.querySelector('#contenedorCard');
+    console.log(contenedorCard)
+    const cardContainer=document.getElementById('contenedorCard');
+    artVentas.forEach((producto)=>{
+        //creamos el elemento
+        const card=document.createElement('div')
+        card.className='card-body'
+        //asignarle algo a la card
+        card.innerHTML=`
+        <h4 class="card-title">${producto.descripcion}</h4>
+        <img src=${producto.imagen} alt="${producto.descripcion}">
+        <p class="card-text">$${producto.precio}</p>
+       
+        <p class="card-text"></p>
+
+         <a href="" class="btn btn-primary">Agregar</a>
+        
+      </div>`
+
+        //Tercer paso
+        cardContainer.append(card)
+    } );
 
 
 
@@ -48,6 +83,7 @@ function ValidarRegistro(nombre,apellido,usuario,password,password2)
     if(nombre === '' || apellido === ''||usuario===''||password==='')
     {
         alert("Uno de los campos está vacío");
+        
        return false;
     }
     else if((usuSistema.some((variable)=>variable.user== usuario)==true) )
@@ -109,6 +145,8 @@ function tieneMinusculas(texto){
     }
     return 0;
  }
+
+
 
 
 function ValidarIngreso(usuarioIngresado,passIngresado)
@@ -200,9 +238,20 @@ else
 
 }
 
+
+
+
+
+
+
+
+
+
 function recogerDatos(){
     let usuIngresado=document.getElementById("IngresoUser").value;
     let IngresoPass=document.getElementById("IngresoPass").value;
+    
+
     
 
     
@@ -214,28 +263,11 @@ if( ValidarIngreso(usuIngresado,IngresoPass)==false)
   }
   else
   {
+  
     window.location.href = "Paginas/Tienda.html";
+  
 
-    const contCard=document.querySelector('#cardcont')
-
-    artVentas.forEach((prod)=>{
-        const card=document.createElement('div')
-        card.className='card-body'
-        card.innerHTML=`<h4 class="card-title">${prod.descripcion}</h4>
-        <img src=${prod.imagen}" class="card-img-top" alt="Let It Be Special Edition – Standard LP">
-       
-        <p class="card-text">${prod.precio}</p>
-
-         <a href="" class="btn btn-primary">Agregar</a>
-        
-      </div>
-    </div>     
-         `
-        contCard.append(card)
-    })
-
-    
-  }
+    }
   
   
   
