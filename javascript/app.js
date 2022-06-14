5dd7bd3e2e5eb9eafbb9de9e0762d38be24abca9
lcedrez <102386169+lcedrez@users.noreply.github.com>
2022-06-14 19:30:51 -0300

    felx

--- javascript/app.js
+++ javascript/app.js
@@ -4,9 +4,9 @@
 
 
 let totalFinal=0
-const artVentas=[articulo1,articulo2,articulo3,articulo4,articulo5,articulo6,articulo7,articulo8]
+const artVentas=[articulo1,articulo2,articulo3,articulo4,articulo5,articulo6,articulo7,articulo8,articulo9,articulo10,articulo11,articulo12]
 const usuSistema=[usuario1,usuario2,usuario3,usuario4]
-const carrito=[]
+let carrito=[]
 
 
 
@@ -14,9 +14,10 @@ const carrito=[]
 //Query De Elementos
 //-------------------------------------------------------------------------------------------------------
 
-const listadoArticulos=document.querySelector('#ContCards')
+const listadoArticulos=document.querySelector('.container')
+const cardContainer=document.getElementById('container');
   
-  
+  console.log(cardContainer)
 
 
 
@@ -26,19 +27,23 @@ const renderizarListaArticulos=()=>{
     artVentas.forEach((arti)=>{
         const artContenedor=document.createElement('div')
         artContenedor.className='card-body'
-        artContenedor.innerHTML=`<h4 class="card-title">${arti.descripcion}</h4>
-                    <img src=${arti.imagen} alt="${arti.descripcion}">
-                    <p class="card-text">$${arti.precio}</p>
+        artContenedor.innerHTML=`
+                    <div class="card-body">
                     
-                    
-                    <p class="card-text"></p>
-                    <button codigo="${arti.cod_articulo}" type="button" class="btn btn-primary"> Agregar al Carrito</button>
-                    `
+                        <h4 class="card-title">${arti.descripcion}</h4>
+                                            <img src=${arti.imagen} alt="${arti.descripcion}">
+                                            <p class="card-text">$${arti.precio}</p>
+                                            
+                                            
+                                            <p class="card-text"></p>
+                                            <button codigo="${arti.cod_articulo}" type="button" class="btn btn-primary"> Agregar al Carrito</button>
+                    </div>  
+                
+        `
                     //Una vez insertado esto debemos agregarlo a la constante creada
                     listadoArticulos.append(artContenedor)
     })
 }
-
 
 
 //EventListeners    
--- javascript/datos.js
+++ javascript/datos.js
@@ -6,6 +6,11 @@ const articulo5=new Articulo("5544","Beatles Playing Cards - Special Edition",12
 const articulo6=new Articulo("6644","Abbey Road Ukulele",17500,0,"Imagenes/E-Commerce/UkuleleAbbey.jfif")
 const articulo7=new Articulo("7744","Yellow Submarine Port Hole Beanie",1500,0,"Imagenes/E-Commerce/gorroYsub.jfif")
 const articulo8=new Articulo("8844","I Love The Beatles Pin",200,0,"Imagenes/E-Commerce/PIN.jfif")
+const articulo9= new Articulo("9944","Launchera - The Beatles",8250,0,"Imagenes/E-Commerce/Launchera.jfif")
+const articulo10=new Articulo("1155","Hornby London Bus",1750,0,"Imagenes/E-Commerce/BusBeatles.jpg")
+const articulo11=new Articulo("2255","Let It Be Cookie Jar",1750,0,"Imagenes/E-Commerce/letitbecockjair.jfif")
+const articulo12=new Articulo("3355","Let It Be Embroidered Patch Olive Beanie",1360,0,"Imagenes/E-Commerce/Gorralet.jfif")
+const articulo13=new Articulo("4455","Let It Be Color Faces Youth Raglan",200,0,"Imagenes/E-Commerce/Y3CTBE219.jfif")
 
 
 const usuario1=new Usuario("Luciano","Cedrez","lcedrez","Luciano1234")
